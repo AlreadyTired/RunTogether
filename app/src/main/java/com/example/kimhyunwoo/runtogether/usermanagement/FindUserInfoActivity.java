1cd4ba2c81dd7843411f4474a91a19275e1f4349
AlreadyTired <gusdndkrl@gmail.com>
2018-07-24 17:30:53 -0700

    LoginActivity.java 에 69번쨰 줄에 Intent 를 삭제해야합니다. 무조건!

--- app/src/main/java/com/example/kimhyunwoo/runtogether/usermanagement/FindUserInfoActivity.java
+++ app/src/main/java/com/example/kimhyunwoo/runtogether/usermanagement/FindUserInfoActivity.java
@@ -12,5 +12,6 @@ public class FindUserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user_info);
+
     }
 }
