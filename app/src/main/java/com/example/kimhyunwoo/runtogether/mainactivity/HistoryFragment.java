060cd2b33bdd352b370f28c500f05c25a4d4077e
AlreadyTired <gusdndkrl@gmail.com>
2018-08-02 10:59:34 -0700

    히스토리컬 프래그먼트에 차트를 띄웠따

--- app/src/main/java/com/example/kimhyunwoo/runtogether/mainactivity/HistoryFragment.java
+++ app/src/main/java/com/example/kimhyunwoo/runtogether/mainactivity/HistoryFragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 import com.example.kimhyunwoo.runtogether.R;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -25,84 +28,39 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HistoryFragment extends Fragment {
+public class HistoryFragment extends SimpleFragment {
 
+    public static Fragment newInstance(){ return new HistoryFragment();}
 
-    private Typeface tf;
-
-    public HistoryFragment() {
-        // Required empty public constructor
-    }
-
+    private LineChart mChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        tf = Typeface.createFromAsset(getActivity().getAssets(),"OpenSans-Regular.ttf");
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.fragment_friends,container,false);
-//
-//        LineChart lineChart = (LineChart)view.findViewById(R.id.myLineChart);
-//
-//        Description description = new Description();
-//        description.setText("YongSun");
-//        lineChart.setDescription(description);
-//
-//        LineDataSet lineDataSet = new LineDataSet(getDataSet(),"My Line Chart");
-//        lineDataSet.setDrawFilled(true);
-//        lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-//
-//        LineData lineData = new LineData(lineDataSet);
-//        lineData.setValueFormatter(new ReportChartXAxisValueFormatter(getXAisValues()));
-//
-//        lineChart.setData(lineData);
-//        lineChart.animateXY(2000,2000);
-//        lineChart.invalidate();
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_history, container, false);
 
-    private List<Entry> getDataSet()
-    {
-        List<Entry> entries = new ArrayList<Entry>();
-        entries.add(new Entry(4f,0));
-        entries.add(new Entry(8f,1));
-        entries.add(new Entry(6f,2));
-        entries.add(new Entry(12f,3));
-        entries.add(new Entry(18f,4));
-        entries.add(new Entry(9f,5));
-        return entries;
-    }
+        mChart = v.findViewById(R.id.myLineChart);
 
-    private List<String> getXAisValues()
-    {
-        List<String> xAxis = new ArrayList<String>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
-        return xAxis;
-    }
+        mChart.getDescription().setEnabled(false);
 
-    private class ReportChartXAxisValueFormatter implements IValueFormatter
-    {
-        private List<String> labels;
+        mChart.setDrawGridBackground(false);
 
-        public ReportChartXAxisValueFormatter(List<String> labels)
-        {
-            this.labels = labels;
-        }
-        @Override
-        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
-            try{
-                int index = (int) value;
-                return this.labels.get(index);
-            }catch (Exception e)
-            {
-                return null;
-            }
-        }
+        mChart.setData(getComplexity());
+        mChart.animateX(3000);
+
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(),"OpenSans-Light.ttf");
+
+        Legend l = mChart.getLegend();
+        l.setTypeface(tf);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTypeface(tf);
+
+        mChart.getAxisRight().setEnabled(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        return v;
     }
 }
