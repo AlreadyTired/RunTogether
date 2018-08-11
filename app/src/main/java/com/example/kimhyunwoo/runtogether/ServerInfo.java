f3f74d2a11f8c06dfefd0deeb02f48d2ab10f148
AlreadyTired <gusdndkrl@gmail.com>
2018-08-10 18:32:34 -0700

    AllUserRealTimeDataViewActivity.java 구축 테스팅 필요함!

--- app/src/main/java/com/example/kimhyunwoo/runtogether/ServerInfo.java
+++ app/src/main/java/com/example/kimhyunwoo/runtogether/ServerInfo.java
@@ -16,4 +16,6 @@ public class ServerInfo {
     public static final String SensorRegistrationURL = "api/sensor/registration";
     public static final String DataTransferURL = "api/sensor/real/transfer";
     public static final String AqiDataTransferURL = "api/sensor/aqi/transfer";
+    public static final String UserListRequestURL = "";
+    public static final String SpecificUserRealTimeDataURL = "";
 }
